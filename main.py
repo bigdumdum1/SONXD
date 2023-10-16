d6875f3547abfd765eea0cf5c29ff48d8e4c2927
Student <kacper.wojcik@edu.uekat.pl>
2023-10-16 12:36:59 +0200

    Glowna funkcja, do poprawy

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("szanująca się grupa")
+def main():
+    data = []
+    text = input("Wprowadz tekst: ")
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if any(text[i] in d for d in data):
+                #do naprawy data[][text[i]] += 1
+            else:
+                data.append({text[i]: 1})
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
