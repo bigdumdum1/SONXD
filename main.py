38030f8306ce393f697def3fbf752d73beaefc16
Student <kacper.wojcik@edu.uekat.pl>
2023-10-16 12:52:27 +0200

    Glowna funkcja do poprawy

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+def main():
+    data = []
+    text = input("Wprowadz tekst: ")
+    # do poprawy
+    for i in range(len(text)):
+        if text[i].isalpha():
+            if (text[i] in d for d in data):
+                data[0][text[i]]
+            else:
+                data.append({text[i]: 1})
+
 def Z_pliku(file):
     plik = open(file)
     dane = plik.read()
@@ -5,3 +16,6 @@ def Z_pliku(file):
     return dane
 
 print(Z_pliku("plik.txt"))
+
+if __name__ == "__main__":
+    main()
